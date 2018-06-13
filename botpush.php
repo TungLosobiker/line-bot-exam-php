bbc1604fb24a28eb6bed7e40a31dfeb1662ce2d0
TungLosobiker <32905712+TungLosobiker@users.noreply.github.com>
2018-06-13 15:14:58 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,7 +4,7 @@
 
 require "vendor/autoload.php";
 
-$access_token = '/Xo13Hv+K4I0wB4MrJ2WJk50C5iK0qrhmNRezfEU9CF94wvI+fGy5xfK5dXrUKN4+kIdYt2ezupPR6K8X3Jh5Ppmo+9a3ePfeR52YsraYcJHMszAOdm7JhY6blFBXnn+tn0nFhOf5UlGX/Q0trutRAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'M+hfTMqp7McHzM38rEyKysTDGZ6oazoJ2hs/mxy9GiyX+bEEDhmYabyPdp7LNN7T+kIdYt2ezupPR6K8X3Jh5Ppmo+9a3ePfeR52YsraYcL2IvFTKiXLf549mmxOyCiM2Cs3WMAWxNnj/Txc+Ed9RgdB04t89/1O/w1cDnyilFU=';
 
 $channelSecret = 'b47115d6374191e166c37263d8cd5e34';
 
