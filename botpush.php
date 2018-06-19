f57e0c519d4373dc0bd6148a6d94f6ba3167de30
Tung <32905712+TungLosobiker@users.noreply.github.com>
2018-06-19 14:47:14 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -8,12 +8,12 @@ $access_token = '+lF6cWUpmr1UiBQSoc3xX8nKV4mMj093J9JF/pQGtpd8XmvJqcD+uyZXs/3B0J8
 
 $channelSecret = 'b47115d6374191e166c37263d8cd5e34';
 
-$pushID = 'U09ca9891ce026843549a511cdf5dec89';
+$pushID = 'U0ac46dd528b9af0c99a61650352de67f';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Hello 19/06/2561');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Hello 19/06/2561 KIMMMMMMM');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
